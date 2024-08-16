146840bd95e985f3cacc5e629cadbc44df01e1d5
KyryloZinkevych <83723120+kyrylo2626@users.noreply.github.com>
2024-08-16 05:31:11 +0300

    Update Location.service.ts

--- client/src/services/Location.service.ts
+++ client/src/services/Location.service.ts
@@ -6,10 +6,7 @@ class LocationService {
 
     private URL = import.meta.env.VITE_LOCATION;
 
-    async getLocation() { const res = axios.get<ILocation>(this.URL);
-                        res.then(response => console.log(response)).catch(error => console.error(error));
-                         axios.get('https://jsonplaceholder.typicode.com/posts').then(response => console.log(response)).catch(error => console.error('Error:', error));
-                        return res}
+    async getLocation() { return axios.get<ILocation>(this.URL) }
 
 }
 
