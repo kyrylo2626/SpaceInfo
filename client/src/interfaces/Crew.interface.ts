4446e2daea1b5fc71fbd7d2ea663f98667638c5a
KyryloZinkevych <83723120+kyrylo2626@users.noreply.github.com>
2024-08-16 04:10:50 +0300

    Delete client directory

--- client/src/interfaces/Crew.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface ICrew {
-    message: string,
-    number: number,
-    people: [ { name: string, craft: string } ]
-}
-
-export interface IISSCrew extends Pick<ICrew, 'number' | 'people'> {}
--- client/src/interfaces/Location.interface.ts
+++
@@ -1,8 +0,0 @@
-export interface ILocation {
-    message: string, 
-    timestamp: number, 
-    iss_position: {
-        latitude: string, 
-        longitude: string
-    }
-}
